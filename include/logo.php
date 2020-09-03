bc216190929c62c61a50ac832ff65566a8d5b0df
Igor <iip2205@mail.ru>
2020-09-03 23:15:43 +0300

    Компонент включаемой области
    Добавление логотипа

---
+++ include/logo.php
@@ -0,0 +1,3 @@
+<div class="logo">
+<a href="/"><img alt="logo.png" src="/upload/medialibrary/f55/f55c31dc2e62cc0b39ee0aba7d6edd80.png" title="logo.png" class="img-responsive" width="164" height="51"></a>
+</div>
